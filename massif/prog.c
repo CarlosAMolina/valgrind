37267e85e19f8a7ea8bb270e561a9755eb3d5bc8
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-08-17 21:47:49 +0200

    Add comments

--- massif/prog.c
+++ massif/prog.c
@@ -16,17 +16,25 @@ int main(void)
    int i;
    int* a[10];
 
+   // Alloc 10.000 B.
    for (i = 0; i < 10; i++) {
       a[i] = malloc(1000);
    }
+   // Total: 10.000 B.
 
+   // Alloc 6.000 B.
    f();
+   // Total: 16.000 B.
 
+   // Alloc 4.000 B.
    g();
+   // Total: 20.000 B.
 
+   // Free 10.000 B.
    for (i = 0; i < 10; i++) {
       free(a[i]);
    }
+   // Total: 10.000 B.
 
    return 0;
 }
